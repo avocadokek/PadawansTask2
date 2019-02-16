b1cd236406f2112ebe9035d3b83bb2d0329e9391
avocadokek <47501889+avocadokek@users.noreply.github.com>
2019-02-16 11:39:01 +0200

    Add files via upload

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -2,12 +2,29 @@
 
 namespace PadawansTask2
 {
+
     public static class ArithmeticSequence
     {
+
         public static int Calculate(int number, int add, int count)
         {
-            // put your code here
+            int Sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                Sum += (number + i * add);
+            }
+
+            return Sum;
             throw new NotImplementedException();
+        }
+
+        public static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            var result = Calculate(a, b, c);
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
